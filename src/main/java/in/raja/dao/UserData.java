4f6fdf0959f0ba19869eff6e33a426d408083291
kris2643 <kpraja965@gmail.com>
2021-05-24 17:29:04 +0530

    Rectified Build Issues

---
+++ src/main/java/in/raja/dao/UserData.java
@@ -0,0 +1,31 @@
+package in.raja.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import in.raja.model.UserDetails;
+
+public class UserData {
+
+	public UserData() {
+		/**
+		 * constructor
+		 */
+	}
+	
+	/*
+	 * Create array list to store all users
+	 */
+	private static List<UserDetails> userList = new ArrayList<>();
+
+	public static void addUser(UserDetails user) {
+		userList.add(user);
+		System.out.println("details added"+userList);
+		
+	}
+	
+   
+    public static List<UserDetails> getUsers1() {
+    	return userList;
+    }
+}
