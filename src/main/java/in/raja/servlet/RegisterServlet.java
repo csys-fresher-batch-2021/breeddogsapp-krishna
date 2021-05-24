4459fb9688d3fd3e7a0f0cd0a9a2460cf9394dde
kris2643 <kpraja965@gmail.com>
2021-05-24 16:05:45 +0530

    Feature1-DogRegistration

---
+++ src/main/java/in/raja/servlet/RegisterServlet.java
@@ -0,0 +1,48 @@
+package in.raja.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import in.raja.dao.UserData;
+import in.raja.model.UserDetails;
+import in.raja.validate.UserValidate;
+
+/**
+ * Servlet implementation class RegisterServlet
+ */
+@WebServlet("/RegisterServlet")
+public class RegisterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String username = request.getParameter("userName");
+		String userMail = request.getParameter("userMail");
+		String password1 = request.getParameter("password1");
+		String password2 = request.getParameter("password2");
+		String phoneNumber = request.getParameter("phoneNumber");
+
+		
+		boolean isValid = UserValidate.validateUser(username, userMail, password1, password2, phoneNumber);
+		
+	
+		
+
+		if (isValid) {
+
+			
+			response.sendRedirect("Login.jsp");
+		} else {
+			response.sendRedirect("Register.jsp?errorMessage=Invalid Login Credentials");
+		}
+	}
+}
---
+++ src/main/java/in/raja/validate/UserValidate.java
@@ -0,0 +1,37 @@
+package in.raja.validate;
+
+import in.raja.dao.UserData;
+import in.raja.model.UserDetails;
+import in.raja.service.UserService;
+
+public class UserValidate {
+	public static boolean validateUser(String username,String userMail,String password1,String password2,String phoneNumber) {
+	boolean isValid = false;
+	//if (!username.equals(null) && !userMail.equals(null) && !password1.equals(null) && !password2.equals(null) && phoneNumber.equals(null)) {
+		//System.out.println("valid");
+	long mobileNo = Long.parseLong(phoneNumber);
+
+		if (password1.equals(password2) && phoneNumber.length() == 10 && password1.trim().length() >= 5) {
+			UserDetails user = new UserDetails(username, userMail, password1, mobileNo);
+			UserData.addUser(user);
+			
+			isValid=true;
+		}
+	//}
+	return isValid;
+	}
+	
+
+	public static boolean checkUser(String phonenumber, String passWord) {
+		long mobileNo = Long.parseLong(phonenumber);
+		boolean isValid = false;
+		if (phonenumber.length() == 10 && mobileNo % 1000000000 != 0 && passWord.trim().length() >= 5) {
+			isValid = UserService.checkUser(phonenumber, passWord);
+		}
+		return isValid;
+		
+
+	}
+	}
+
+	
