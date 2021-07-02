89c779bd59f7c622cecdd872a230b379543c55e6
Krishna Prasad Raja S <kpraja965@gmail.com>
2021-07-02 22:42:06 +0530

    sql files added

---
+++ sql/V3_placeorder_dogs.sql
@@ -0,0 +1,8 @@
+create table placeorder_dogs(order_dogno int , orderuser_phoneno bigint , orderuser_address varchar(25) not null 
+					   , status varchar(25) not null , user_id int
+					   , order_id serial primary key   , 
+							 order_date timestamp without time zone ,
+							 delivery_date  timestamp without time zone );
+							 
+select * from placeorder_dogs;
+ drop table placeorder_dogs;
